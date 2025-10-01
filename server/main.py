7e5902538b340ffdf97e95e9368ac37d0ce74321
Tewodros Cheru <80104928+teddycheru@users.noreply.github.com>
2025-10-01 15:51:32 +0300

    Updating Frontend URL

--- server/main.py
+++ server/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # React dev server
+    allow_origins=["https://tender-trend.vercel.app/"],  # React dev server (Vercel)
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
