3893f92fb49a1458bcf0706e99cebafa7c254d0d
Tewodros Cheru <80104928+teddycheru@users.noreply.github.com>
2025-10-01 16:07:56 +0300

    Update main.py

--- server/main.py
+++ server/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://tender-trend.vercel.app/"],  # React dev server (Vercel)
+    allow_origins=["https://tender-trend.vercel.app"],  # React dev server (Vercel)
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
